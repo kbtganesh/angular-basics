723b6dbd34f8742f8bb1a640b3a3158b0339c912
Murugan <Ganeshbabu.Murugan@agilysys.com>
2019-01-24 11:56:00 +0530

    Add & Delete apis added

--- src/app/cast.service.ts
+++ src/app/cast.service.ts
@@ -50,6 +50,34 @@ export class CastService {
     );
   }
 
+  addCast (cast: Cast): Observable<Cast> {
+    return this.http.post<Cast>(this.heroesUrl, cast, httpOptions)
+      .pipe(
+        tap(hero => this.log(`added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Cast>('addCast'))
+      );
+  }
+
+  deleteCast(id: number) {
+    const url = this.heroesUrl + `/${id}`;
+    return this.http.delete(url, httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Cast>('deleteHero'))
+      );
+  }
+
+  searchCastings(term: string): Observable<Cast[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Cast[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Cast[]>('searchHeroes', []))
+    );
+  }
+
   /**
  * Handle Http operation that failed.
  * Let the app continue.
--- src/app/cast/cast.component.ts
+++ src/app/cast/cast.component.ts
@@ -26,4 +26,21 @@ export class CastComponent implements OnInit {
     this.castService.getCastings().subscribe(castings => this.castings = castings);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.castService.addCast({ name } as Cast)
+      .subscribe(cast => {
+        this.castings.push(cast);
+      });
+  }
+
+  delete(cast: Cast) {
+    const id = cast.id;
+    this.castService.deleteCast(id).subscribe(res => {
+      console.log('res', res);
+      this.castings = this.castings.filter(c => c.id !== id);
+    });
+  }
+
 }
